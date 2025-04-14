44cc7a6aa54c299cf482548578906354156fad54
Anthe Sevenants <anthe.sevenants@kuleuven.be>
2025-04-14 14:20:40 +0200

    Add average vocabulary computation to model datacollector

--- model/model.py
+++ model/model.py
@@ -4,7 +4,7 @@ import math
 import numpy as np
 
 from model.agents import ReductionAgent
-from model.helpers import compute_communicative_success, compute_communicative_failure, compute_mean_non_zero_ratio, compute_tokens_chosen, distances_to_probabilities_softmax, distances_to_probabilities_linear, compute_confusion_matrix, compute_average_vocabulary, compute_average_communicative_success_probability, compute_mean_communicative_success_per_token, compute_mean_reduction_per_token, compute_repairs, compute_mean_agent_l1, compute_mean_token_l1, compute_fail_reason, compute_mean_exemplar_count
+from model.helpers import compute_communicative_success, compute_communicative_failure, compute_mean_non_zero_ratio, compute_tokens_chosen, distances_to_probabilities_softmax, distances_to_probabilities_linear, compute_confusion_matrix, compute_average_vocabulary, compute_average_communicative_success_probability, compute_mean_communicative_success_per_token, compute_mean_reduction_per_token, compute_repairs, compute_mean_agent_l1, compute_mean_token_l1, compute_fail_reason, compute_mean_exemplar_count, compute_average_vocabulary_flexible
 from model.types.neighbourhood import NeighbourhoodTypes
 from model.types.production import ProductionModels
 from model.types.reduction import ReductionModes
@@ -82,7 +82,8 @@ class ReductionModel(mesa.Model):
                              "mean_token_l1": compute_mean_token_l1,
                              "confusion_matrix": compute_confusion_matrix,
                              "fail_reason": compute_fail_reason,
-                             "mean_exemplar_count": compute_mean_exemplar_count }
+                             "mean_exemplar_count": compute_mean_exemplar_count,
+                             "average_vocabulary": compute_average_vocabulary_flexible }
         )
 
     def reset(self):
