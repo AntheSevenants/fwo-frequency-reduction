7704783c77e5066139c9cef54c42e0bb18ce9cc3
Anthe Sevenants <anthe.sevenants@kuleuven.be>
2025-04-14 15:11:17 +0200

    Only take datacollector snapshots every n steps to reduce computational load

--- model/model.py
+++ model/model.py
@@ -15,7 +15,7 @@ from model.types.repair import Repair
 class ReductionModel(mesa.Model):
     """A model of Joan Bybee's *reducing effect*"""
 
-    def __init__(self, num_agents=50, vectors=[], tokens=[], frequencies=[], percentiles=[], ranks=[], reduction_prior = 0.5, memory_size=1000, initial_token_count=2, prefill_memory=True, disable_reduction=False, neighbourhood_type=NeighbourhoodTypes.SPATIAL, neighbourhood_size=0.5, production_model=ProductionModels.SINGLE_EXEMPLAR, reduction_mode=ReductionModes.ALWAYS, feedback_type=FeedbackTypes.FEEDBACK, repair=Repair.NO_REPAIR, seed=None):
+    def __init__(self, num_agents=50, vectors=[], tokens=[], frequencies=[], percentiles=[], ranks=[], reduction_prior = 0.5, memory_size=1000, initial_token_count=2, prefill_memory=True, disable_reduction=False, neighbourhood_type=NeighbourhoodTypes.SPATIAL, neighbourhood_size=0.5, production_model=ProductionModels.SINGLE_EXEMPLAR, reduction_mode=ReductionModes.ALWAYS, feedback_type=FeedbackTypes.FEEDBACK, repair=Repair.NO_REPAIR, datacollector_step_size=100, seed=None):
         super().__init__(seed=seed)
 
         self.num_agents = num_agents
@@ -26,6 +26,8 @@ class ReductionModel(mesa.Model):
         self.prefill_memory = prefill_memory
         self.seed = seed
         self.current_step = 0
+        self.datacollector_step_size = datacollector_step_size
+
         self.neighbourhood_size = neighbourhood_size
         self.neighbourhood_type = neighbourhood_type
         self.production_model = production_model
@@ -57,7 +59,7 @@ class ReductionModel(mesa.Model):
         # Communication success
         #
         self.confusion_matrix = np.zeros((self.num_tokens, self.num_tokens))
-        self.reset()
+        self.reset(force=True)
         self.turns = []
 
         self.tokens_chosen = { token: 0 for token in self.tokens }
@@ -86,19 +88,23 @@ class ReductionModel(mesa.Model):
                              "average_vocabulary": compute_average_vocabulary_flexible }
         )
 
-    def reset(self):
-        self.total_repairs = 0
-        self.successful_turns = 0
-        self.failed_turns = 0
-        self.total_turns = 0
-        self.fail_reason = { "no_tokens": 0, "wrong_winner": 0, "shared_top": 0 }
+        self.datacollector.collect(self)
+
+    def reset(self, force=False):
+        if self.current_step - 1 % self.datacollector_step_size == 0 or force:
+            self.total_repairs = 0
+            self.successful_turns = 0
+            self.failed_turns = 0
+            self.total_turns = 0
+            self.fail_reason = { "no_tokens": 0, "wrong_winner": 0, "shared_top": 0 }
 
     def step(self):
         self.reset()
         self.agents.do("reset")
         self.agents.shuffle_do("interact_do")
 
-        self.datacollector.collect(self)
+        if self.current_step % self.datacollector_step_size == 0:
+            self.datacollector.collect(self)
         self.current_step += 1
 
     def step_unitary(self):
