0f144ab9efded3a3bcadd054c0fd7528eedbb602
Anthe Sevenants <anthe.sevenants@kuleuven.be>
2025-12-01 12:14:09 +0100

    Disable_title in aggregate visualisations

--- visualisation/aggregate.py
+++ visualisation/aggregate.py
@@ -3,45 +3,66 @@ import matplotlib.pyplot as plt
 import export.aggregate.tools
 
 
-def ratio_bar(parameter_mapping, ax, title):
+def ratio_bar(parameter_mapping, ax, title, disable_title=False):
     return bar(
         parameter_mapping,
         ax,
         ylim=[0, 1],
         title=title,
+        disable_title=disable_title
     )
 
-def ratio_stem(parameter_mapping, ax, title, log=False):
+def ratio_stem(parameter_mapping, ax, title, log=False, disable_title=False):
     return stem(
         parameter_mapping,
         ax,
         ylim=[0, 1],
         title=title,
-        log=log
+        log=log,
+        disable_title=disable_title
     )
 
-def communicative_success(parameter_mapping, ax):
+def communicative_success(parameter_mapping, ax, disable_title=False):
     return ratio_stem(
-        parameter_mapping, ax, "Communicative success across selected parameter"
+        parameter_mapping,
+        ax,
+        "Communicative success across selected parameter",
+        disable_title=disable_title
     )
 
 
-def communicative_success_macro(parameter_mapping, ax):
+def communicative_success_macro(parameter_mapping, ax, disable_title=False):
     return ratio_bar(
-        parameter_mapping, ax, "Macro communicative success across selected parameter"
+        parameter_mapping,
+        ax,
+        "Macro communicative success across selected parameter",
+        disable_title=disable_title
     )
 
-def mean_l1(parameter_mapping, ax):
+def mean_l1(parameter_mapping, ax, disable_title=False):
     return bar(
-        parameter_mapping, ax, "Mean L1 across selected parameter"
+        parameter_mapping,
+        ax,
+        "Mean L1 across selected parameter",
+        disable_title=disable_title
     )
 
+def entropy(parameter_mapping, ax, disable_title=False):
+    return ratio_stem(
+        parameter_mapping,
+        ax,
+        "L1 entropy across elected parameter",
+        disable_title=disable_title
+    )
 
-def bar(parameter_mapping, ax, ylim=None, title=None):
+def bar(parameter_mapping, ax, ylim=None, title=None, disable_title=False):
     parameter_mapping = export.aggregate.tools.mean(parameter_mapping)
 
     x = list(parameter_mapping.keys())
     ax.bar(x, list(parameter_mapping.values()))
+
+    if disable_title:
+        title = None
 
     # Make sure string x ticks get rendered correctly
     if type(x[0]) == str:
@@ -59,7 +80,7 @@ def bar(parameter_mapping, ax, ylim=None, title=None):
 
     return ax
 
-def stem(parameter_mapping, ax, ylim=None, title=None, log=False):
+def stem(parameter_mapping, ax, ylim=None, title=None, log=False, disable_title=False):
     parameter_mapping = export.aggregate.tools.mean(parameter_mapping)
 
     x = list(parameter_mapping.keys())
@@ -67,6 +88,9 @@ def stem(parameter_mapping, ax, ylim=None, title=None, log=False):
     if log:
         ax.set_xscale("log")
     ax.stem(x, list(parameter_mapping.values()))
+
+    if disable_title:
+        title = None
 
     # Make sure string x ticks get rendered correctly
     if type(x[0]) == str:
