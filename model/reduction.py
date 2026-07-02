39f16afab9d586d44036a8484bf400ec732df54d
Anthe Sevenants <anthe.sevenants@kuleuven.be>
2026-07-02 15:34:17 +0200

    Add a new reduction method

--- model/reduction.py
+++ model/reduction.py
@@ -18,4 +18,27 @@ class SoftThresholding(Base):
         return input_vector - self.strength
 
 
+@dataclass
+class CompressReduce(Base):
+    decay_factor: float = 0.95
+    homogenisation_factor: float = 0.1
+
+    def get_reduced_vector(self, input_vector: np.ndarray):
+        # We calculate the mean of the vector to get the "centre" of the identity
+        centre = np.mean(input_vector)
+
+        # Bring vectors closer together by adding a small amount of the difference with the centre
+        output_vector = input_vector + (
+            self.homogenisation_factor * (centre - input_vector)
+        )
+
+        # Now, also shrink the magnitude of the vector
+        output_vector *= self.decay_factor
+
+        # Prevent float bs
+        output_vector = np.round(output_vector, 4)
+
+        return output_vector
+
+
 # other reduction is available :-)
