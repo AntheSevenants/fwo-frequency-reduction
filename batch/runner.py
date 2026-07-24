11b191e22b99c7db77f1a9c09ae05dc0ce694495
Anthe Sevenants <anthe.sevenants@kuleuven.be>
2026-07-24 11:53:45 +0200

    Move to BatchProfile instantiations

--- batch/runner.py
+++ batch/runner.py
@@ -48,6 +48,7 @@ from tqdm.auto import tqdm
 from mesa.model import Model
 
 from model.model import ReductionModel
+from batch.profile import BatchProfile
 from batch.params import dict_to_params
 
 multiprocessing.set_start_method("spawn", force=True)
@@ -73,7 +74,7 @@ def batch_run(
     model_cls: type[ReductionModel],
     sweeps_dir: str,
     current_sweep: str,
-    parameters: Mapping[str, Any | Iterable[Any]],
+    parameters: BatchProfile,
     # We still retain the Optional[int] because users may set it to None (i.e. use all CPUs)
     number_processes: int | None = 1,
     iterations: int = 1,
@@ -111,29 +112,31 @@ def batch_run(
     # Makes it MUCH easier to find the different parameter combinations later
     run_id = 0
     combination_id = 0
-    for kwargs in _make_model_kwargs(parameters):
-        for iteration in range(iterations):
-            # We have to make a new kwargs object in order to prevent the seed from sticking into this specific combinatino
-            if "seed" not in kwargs:
-                kwargs_pass = {**kwargs, "seed": random.randint(0, 99999999)}
-            else:
-                kwargs_pass = kwargs
+    for parameter_set in parameters.param_sets:
+        for kwargs in _make_model_kwargs(parameters.param_sets[parameter_set]):
+            for iteration in range(iterations):
+                # We have to make a new kwargs object in order to prevent the seed from sticking into this specific combination
+                if "seed" not in kwargs:
+                    kwargs_pass = {**kwargs, "seed": random.randint(0, 99999999)}
+                else:
+                    kwargs_pass = kwargs
 
-            # Set the datacollector step size dynamically based on the max steps
-            kwargs_pass["datacollector_step_size"] = datacollector_step_size
+                # Set the datacollector step size dynamically based on the max steps
+                kwargs_pass["datacollector_step_size"] = datacollector_step_size
 
-            runs_list.append(
-                (
-                    run_id,
-                    combination_id,
-                    iteration,
-                    sweeps_dir,
-                    current_sweep,
-                    kwargs_pass,
+                runs_list.append(
+                    (
+                        run_id,
+                        parameter_set,
+                        combination_id,
+                        iteration,
+                        sweeps_dir,
+                        current_sweep,
+                        kwargs_pass,
+                    )
                 )
-            )
-            run_id += 1
-        combination_id += 1
+                run_id += 1
+            combination_id += 1
 
     process_func = partial(
         _model_run_func,
@@ -266,7 +269,7 @@ def check_webhook_update(
 
 def _model_run_func(
     model_cls: type[ReductionModel],
-    run: tuple[int, int, int, str, str, dict[str, Any]],
+    run: tuple[int, str, int, int, str, str, dict[str, Any]],
     max_steps: int,
     iterations: int,
     data_collection_period: int,
@@ -291,7 +294,15 @@ def _model_run_func(
     List[Dict[str, Any]]
         Return model_data, agent_data from the reporters
     """
-    run_id, combination_id, iteration, sweeps_dir, current_sweep, kwargs = run
+    (
+        run_id,
+        parameter_set,
+        combination_id,
+        iteration,
+        sweeps_dir,
+        current_sweep,
+        kwargs,
+    ) = run
     model_params = dict_to_params(kwargs)
     model = model_cls(model_params)
     while model.running and model.steps <= max_steps:
@@ -310,6 +321,7 @@ def _model_run_func(
     data = [
         {
             "run_id": run_id,
+            "parameter_set": parameter_set,
             "combination_id": combination_id,
             "iteration": iteration,
             "max_steps": max_steps,
--- model/profiles.py
+++ model/profiles.py
@@ -2,86 +2,50 @@ import model.enums
 import model.sampling
 import model.reduction
 
-# TODO
+from batch.profile import BatchProfile
+
 params = {
-    "regular": {
-        "num_agents": 10,
-        "reduction": [
-            model.reduction.SoftThresholding(strength=1),
-            model.reduction.SoftThresholding(strength=5),
-        ],
-        "reduction_prob": [0.1, 0.5, 1],
-        "value_floor": [1],
-        "vector_bounds": [[70, 100]],
-        "learning_rate": [0.05, 0.2, 0.5, 0.8],
-        "initial_sigma": [5, 10, 15],
-        "reentrance": [False, True],
-    },
-    "quick": {
-        "num_agents": 10,
-        "reduction": [
-            model.reduction.SoftThresholding(strength=1),
-        ],
-        "reduction_prob": [0, 0.1, 0.5, 1],
-        "value_floor": [1],
-        "vector_bounds": [[70, 100]],
-        "learning_rate": [0.2],
-        "initial_sigma": [5],
-        "reentrance": [False, True],
-        "learning_rate": [0.01, 0.2, 0.5],
-    },
-    "quick2": {
-        "num_agents": 10,
-        "reduction": [
-            model.reduction.SoftThresholding(strength=1),
-        ],
-        "reduction_prob": [0.2, 0.5, 0.8],
-        "value_floor": [15],
-        "vector_bounds": [[70, 100]],
-        "learning_rate": [0.2],
-        "initial_sigma": [5],
-        "reentrance_entropy_floor": [0, 0.025, 0.05, 0.1],
-    },
-    "quick3": {
-        "num_agents": 10,
-        "batch_size": 10,
-        "reduction": [
-            model.reduction.CompressReduce(),
-        ],
-        "reduction_prob": [0, 0.5],
-        "value_floor": [15],
-        "vector_bounds": [[70, 100]],
-        "learning_rate": [0.2],
-        "initial_sigma": [5],
-        "reentrance_entropy_floor": [0, 0.05],
-        "priors_enabled": [True, False],
-        "sampling_type": [
-            model.sampling.ZipfianSampling(zipf_param=1),
-            model.sampling.ZipfianSampling(zipf_param=0.6),
-            model.sampling.ZipfianSampling(zipf_param=0.01),
-        ],
-        "feedback_type": [
-            model.enums.FeedbackTypes.NO_FEEDBACK,
-            model.enums.FeedbackTypes.FEEDBACK,
-        ],
-    },
-    "quick4": {
-        "num_agents": 10,
-        "reduction": [
-            model.reduction.CompressReduce(),
-        ],
-        "reduction_prob": [0.5],
-        "value_floor": [15],
-        "vector_bounds": [[70, 100]],
-        "learning_rate": [0.2],
-        "initial_sigma": [5],
-        "discriminatory_entropy_floor": [0, 0.01, 0.025, 0.05],
-        "priors_enabled": [True, False],
-        "sampling_type": [
-            model.sampling.ZipfianSampling(zipf_param=1),
-        ],
-        "feedback_type": [
-            model.enums.FeedbackTypes.NO_FEEDBACK,
-        ],
-    },
+    "article": BatchProfile(
+        parent_params={
+            "num_agents": 10,
+            "reduction": model.reduction.CompressReduce(),
+            "reduction_prob": [0.5],
+            "value_floor": [15],
+            "vector_bounds": [[70, 100]],
+            "learning_rate": [0.2],
+            "initial_sigma": [5],
+            "priors_enabled": True,
+            "sampling_type": model.sampling.ZipfianSampling(zipf_param=1),
+            "feedback_type": model.enums.FeedbackTypes.NO_FEEDBACK,
+            "batch_size": 10,
+        },
+        child_params={
+            "priors-no-priors": {
+                "priors_enabled": [True, False],
+            },
+            "re-entrance": {
+                "reentrance_entropy_floor": [0, 0.05],
+            },
+            "discriminatory-force": {
+                "discriminatory_entropy_floor": [0, 0.01, 0.025, 0.05],
+            },
+            "no-zipfian": {
+                "reentrance_entropy_floor": 0.05,
+                "sampling_type": [
+                    model.sampling.ZipfianSampling(zipf_param=1),
+                    model.sampling.ZipfianSampling(zipf_param=0.6),
+                    model.sampling.ZipfianSampling(zipf_param=0.01),
+                ],
+            },
+            "no-zipfian-feedback": {
+                "reentrance_entropy_floor": 0.05,
+                "sampling_type": [
+                    model.sampling.ZipfianSampling(zipf_param=1),
+                    model.sampling.ZipfianSampling(zipf_param=0.6),
+                    model.sampling.ZipfianSampling(zipf_param=0.01),
+                ],
+                "feedback_type": model.enums.FeedbackTypes.FEEDBACK,
+            },
+        },
+    )
 }
